4b428af9f09c7dbb40f38f063ffa656c6036ef72
gercenioxsilva <xavier_lwl@hotmail.com>
2026-06-24 09:16:01 -0300

    Revert "feat(infra): add ACM certificate and Route53 DNS records for orquestraerp.com.br"
    This reverts commit 362c38a4baed1850b0c4beaa70754b38271d95ba.

--- terraform/static-sites.tf
+++ terraform/static-sites.tf
@@ -105,8 +105,6 @@ resource "aws_cloudfront_distribution" "static_site" {
   default_root_object = "index.html"
   price_class         = "PriceClass_100"
 
-  aliases = each.key == "backoffice" ? [var.domain_name, var.www_domain_name] : []
-
   origin {
     origin_id                = "s3-${each.key}"
     domain_name              = aws_s3_bucket.static_site[each.key].bucket_regional_domain_name
@@ -201,12 +199,10 @@ resource "aws_cloudfront_distribution" "static_site" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.main.certificate_arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    cloudfront_default_certificate = true
   }
 
-  depends_on = [aws_s3_bucket_policy.static_site, aws_acm_certificate_validation.main]
+  depends_on = [aws_s3_bucket_policy.static_site]
 
   tags = {
     Name        = "crm-${each.key}-${var.environment}"
